f1c58725cd9be5f67c0473ec28fed77c89d8a23c
Moustapha <mouhamedjr2012@gmail.com>
2023-04-18 22:24:17 +0000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: "Moustapha",
+      bio: "développeur web",
+      profession: "informaticien",
+      shows: true,
+    };
+  }
 
+  toggle() {
+    this.setState({ shows: !this.state.shows });
+    console.log(this.state.shows);
+  }
 
-class App extends 
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() => this.toggle()}>Toggle</button>
+        {this.state.shows && <img src="/logo192.png" alt="" />}
+        <Clock />
+      </div>
+    );
+  }
+}
 
 export default App;
